8265d546b56c6b8649db5e466dc592d3273dea72
Ditrogen <yohan.loadout@gmail.com>
2023-09-11 14:12:14 +0700

    Window setting
    windowed to fullscreen

--- Scenes/world.gd
+++ Scenes/world.gd
@@ -2,6 +2,25 @@ extends Node2D
 
 var food = preload("res://Scenes/pelet.tscn")
 
+<<<<<<< Updated upstream
+=======
+func _ready():
+	Engine.time_scale = 1	
+ 
+func _process(delta):
+	if Input.is_action_just_pressed("pause"):
+		pauseMenu()
+
+func pauseMenu():
+	if paused:
+		pause_menu.hide()
+		Engine.time_scale = 1
+	else:
+		pause_menu.show()
+		Engine.time_scale = 0
+	
+	paused = !paused
+>>>>>>> Stashed changes
 
 func _on_click_area_pressed():
 	var obj = food.instantiate()
